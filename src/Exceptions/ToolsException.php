551f6b55e9ac6ea179416b0bbe73ab37a287e6cb
fuliang <fuliang19890809@163.com>
2023-04-24 22:40:58 +0800

    提交

--- src/Exceptions/ToolsException.php
+++ src/Exceptions/ToolsException.php
@@ -2,13 +2,31 @@
 
 namespace Fuliang\PhpTools\Exceptions;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
 use Fuliang\PhpTools\Constants\ErrorCode;
 use Throwable;
 
-class ToolsException extends \Exception
+class ToolsException extends \Exception implements PluginInterface
 {
     public function __construct(int $code = ErrorCode::ERROR_400, string $message = "", Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
+    }
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement activate() method.
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
     }
 }
