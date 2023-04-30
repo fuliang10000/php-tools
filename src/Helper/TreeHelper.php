ce7339cc49f7e261aafec6b29aad904b1e0455ad
fuliang <fuliang19890809@163.com>
2023-04-30 17:30:20 +0800

    tree优化

--- src/Helper/TreeHelper.php
+++ src/Helper/TreeHelper.php
@@ -5,87 +5,84 @@ namespace Fuliang\PhpTools\Helper;
 
 class TreeHelper
 {
-    /**
-     * 用于树型数组完成递归格式的全局变量.
-     */
-    protected static $formatTree;
 
     /**
      * 将格式数组转换为基于标题的树（实际还是列表，只是通过在相应字段加前缀实现类似树状结构）.
      * @param array $list
-     * @param int   $level 进行递归时传递用的参数
-     * @param mixed $title
+     * @param int $level 进行递归时传递用的参数
+     * @param string $title
+     * @param array $formatTree 过渡用的中间数组
+     * @return array
      */
-    public static function _toFormatTree($list, $level = 0, $title = 'title')
+    public function _toFormatTree(array $list, int $level = 0, string $title = 'title', array &$formatTree = []): array
     {
         foreach ($list as $key => $val) {
             $title_prefix = str_repeat('--------| ', $level);
-            $val['level']        = $level;
+            $val['level'] = $level;
             $val['title_prefix'] = 0 == $level ? '' : $title_prefix;
-            $val['title_show']   = 0 == $level ? $val[$title] : $title_prefix . $val[$title];
-            if (! array_key_exists('_child', $val)) {
-                array_push(self::$formatTree, $val);
+            $val['title_show'] = 0 == $level ? $val[$title] : $title_prefix . $val[$title];
+            if (!array_key_exists('_child', $val)) {
+                array_push($formatTree, $val);
             } else {
                 $child = $val['_child'];
                 unset($val['_child']);
-                array_push(self::$formatTree, $val);
-                self::_toFormatTree($child, $level + 1, $title); //进行下一层递归
+                array_push($formatTree, $val);
+                $this->_toFormatTree($child, $level + 1, $title); //进行下一层递归
             }
+            return $formatTree;
         }
     }
 
     /**
      * 将格式数组转换为树.
      * @param array $list
-     * @param int   $level  进行递归时传递用的参数
-     * @param mixed $title
-     * @param mixed $pk
-     * @param mixed $pid
-     * @param mixed $root
-     * @param mixed $strict
+     * @param string $title
+     * @param string $pk
+     * @param string $pid
+     * @param int $root
+     * @param bool $strict
+     * @return array
      */
-    public static function toFormatTree($list, $title = 'title', $pk = 'id', $pid = 'pid', $root = 0, $strict = true)
+    public function toFormatTree(array $list, string $title = 'title', string $pk = 'id', string $pid = 'pid', int $root = 0, bool $strict = true): array
     {
-        $list             = self::list_to_tree($list, $pk, $pid, '_child', $root, $strict);
-        self::$formatTree = [];
-        self::_toFormatTree($list, 0, $title);
+        $list = $this->listToTree($list, $pk, $pid, '_child', $root, $strict);
+        $formatTree = [];
+        $this->_toFormatTree($list, 0, $title);
 
-        return self::$formatTree;
+        return $formatTree;
     }
 
     /**
      * 将数据集转换成Tree（真正的Tree结构）.
-     * @param  array  $list   要转换的数据集
-     * @param  string $pk     ID标记字段
-     * @param  string $pid    parent标记字段
-     * @param  string $child  子代key名称
-     * @param  string $root   返回的根节点ID
-     * @param  string $strict 默认严格模式
+     * @param array $list 要转换的数据集
+     * @param string $pk ID标记字段
+     * @param string $pid parent标记字段
+     * @param string $child 子代key名称
+     * @param int $root 返回的根节点ID
+     * @param bool $strict 默认严格模式
      * @return array
      */
-    public static function list_to_tree($list, $pk = 'id', $pid = 'pid', $child = '_child', $root = 0, $strict = true)
+    public function listToTree(array $list, string $pk = 'id', string $pid = 'pid', string $child = '_child', int $root = 0, bool $strict = true): array
     {
         // 创建Tree
         $tree = [];
-        if (is_array($list)) {
-            // 创建基于主键的数组引用
-            $refer = [];
-            foreach ($list as $key => $data) {
-                $refer[$data[$pk]] = &$list[$key];
-            }
-            foreach ($list as $key => $data) {
-                // 判断是否存在parent
-                $parent_id = $data[$pid];
-                if (null === $parent_id || (string) $root === $parent_id) {
-                    $tree[] = &$list[$key];
+        // 创建基于主键的数组引用
+        $refer = [];
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] = &$list[$key];
+        }
+        foreach ($list as $key => $data) {
+            // 判断是否存在parent
+            $parent_id = $data[$pid];
+            if (null === $parent_id || (string)$root === $parent_id) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parent_id])) {
+                    $parent = &$refer[$parent_id];
+                    $parent[$child][] = &$list[$key];
                 } else {
-                    if (isset($refer[$parent_id])) {
-                        $parent           = &$refer[$parent_id];
-                        $parent[$child][] = &$list[$key];
-                    } else {
-                        if (false === $strict) {
-                            $tree[] = &$list[$key];
-                        }
+                    if (false === $strict) {
+                        $tree[] = &$list[$key];
                     }
                 }
             }
@@ -95,25 +92,25 @@ class TreeHelper
     }
 
     /**
-     * 将list_to_tree的树还原成列表.
-     * @param  array  $tree  原来的树
-     * @param  string $child 孩子节点的键
-     * @param  string $order 排序显示的键，一般是主键 升序排列
-     * @param  array  $list  过渡用的中间数组，
+     * 将listToTree的树还原成列表.
+     * @param array $tree 原来的树
+     * @param string $child 孩子节点的键
+     * @param string $order 排序显示的键，一般是主键 升序排列
+     * @param array $list 过渡用的中间数组，
      * @return array  返回排过序的列表数组
      */
-    public static function tree_to_list($tree, $child = '_child', $order = 'id', &$list = [])
+    public function treeToList(array $tree, string $child = '_child', string $order = 'id', array &$list = []): array
     {
         if (is_array($tree)) {
             foreach ($tree as $key => $value) {
                 $reffer = $value;
                 if (isset($reffer[$child])) {
                     unset($reffer[$child]);
-                    self::tree_to_list($value[$child], $child, $order, $list);
+                    $this->treeToList($value[$child], $child, $order, $list);
                 }
                 $list[] = $reffer;
             }
-            $list = self::list_sort_by($list, $order, $sortby = 'asc');
+            $list = $this->listSortBy($list, $order, $sortBy = 'asc');
         }
 
         return $list;
@@ -121,67 +118,30 @@ class TreeHelper
 
     /**
      * 对查询结果集进行排序.
-     * @param  array  $list   查询结果
-     * @param  string $field  排序的字段名
-     * @param  array  $sortby 排序类型 asc正向排序 desc逆向排序 nat自然排序
+     * @param array $list 查询结果
+     * @param string $field 排序的字段名
+     * @param string $sortBy 排序类型 asc正向排序 desc逆向排序 nat自然排序
      * @return array
      */
-    public static function list_sort_by($list, $field, $sortby = 'asc')
+    public function listSortBy(array $list, string $field, string $sortBy = 'asc'): array
     {
-        if (is_array($list)) {
-            $refer = $resultSet = [];
-            foreach ($list as $i => $data) {
-                $refer[$i] = &$data[$field];
-            }
-            switch ($sortby) {
-                case 'asc': // 正向排序
-                    asort($refer);
-                    break;
-                case 'desc':// 逆向排序
-                    arsort($refer);
-                    break;
-                case 'nat': // 自然排序
-                    natcasesort($refer);
-                    break;
-            }
-            foreach ($refer as $key => $val) {
-                $resultSet[] = &$list[$key];
-            }
-
-            return $resultSet;
+        $refer = $resultSet = [];
+        foreach ($list as $i => $data) {
+            $refer[$i] = &$data[$field];
         }
-
-        return false;
-    }
-
-    /**
-     * 在数据列表中搜索.
-     * @param  array $list      数据列表
-     * @param  mixed $condition 查询条件
-     *                          支持 array('name'=>$value) 或者 name=$value
-     * @return array
-     */
-    public function list_search($list, $condition)
-    {
-        if (is_string($condition)) {
-            parse_str($condition, $condition);
+        switch ($sortBy) {
+            case 'asc': // 正向排序
+                asort($refer);
+                break;
+            case 'desc':// 逆向排序
+                arsort($refer);
+                break;
+            case 'nat': // 自然排序
+                natcasesort($refer);
+                break;
         }
-        // 返回的结果集合
-        $resultSet = [];
-        foreach ($list as $key => $data) {
-            $find = false;
-            foreach ($condition as $field => $value) {
-                if (isset($data[$field])) {
-                    if (0 === strpos($value, '/')) {
-                        $find = preg_match($value, $data[$field]);
-                    } elseif ($data[$field] == $value) {
-                        $find = true;
-                    }
-                }
-            }
-            if ($find) {
-                $resultSet[] = &$list[$key];
-            }
+        foreach ($refer as $key => $val) {
+            $resultSet[] = &$list[$key];
         }
 
         return $resultSet;
